1b726834ffea8063f81778bab80433dc426d4a77
yanitski <yanitski@ph1.uni-koeln.de>
2025-06-10 14:35:19 -0600

    allow agent to make function call requests

--- main.py
+++ main.py
@@ -34,11 +34,41 @@ def main():
     model_name = "gemini-2.0-flash-001"
     
     #prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            }
+        ),
+    )
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    system_prompt = '''
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    '''
+
     response = client.models.generate_content(
         model=model_name, 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
     )
     
     # Print prompt if verbose output
@@ -48,6 +78,12 @@ def main():
 
     # Print response
     print(f"Response: {response.text}\n")
+    if len(response.function_calls):
+        for c in response.function_calls:
+            print(f"Calling function: {c.name}({c.args})")
+        print()
+    else:
+        print("No functions calls were requested.\n")
 
     # Print token number if verbose output
     if verbose:
