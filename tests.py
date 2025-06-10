a1e6a8a746d748cf85526a42e686f76de0312df0
yanitski <yanitski@ph1.uni-koeln.de>
2025-06-10 13:47:49 -0600

    tests added for running scripts

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 import unittest
 #from functions.get_files_info import get_files_info
 #from functions.get_file_content import get_file_content
-from functions.write_file import write_file
+#from functions.write_file import write_file
+from functions.run_file import run_python_file
 
 class TestGetFileInfo(unittest.TestCase):
     # get files info
@@ -57,23 +58,48 @@ class TestGetFileInfo(unittest.TestCase):
         #     self.assertIn("Error:", out)
 
     # write file
-    def test_write_lorem(self):
-        out = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-        print("\nlorem.txt")
-        print(out)
-        self.assertIn("Successfully wrote to ", out)
+        # def test_write_lorem(self):
+        #     out = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+        #     print("\nlorem.txt")
+        #     print(out)
+        #     self.assertIn("Successfully wrote to ", out)
 
-    def test_write_morelorem(self):
-        out = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-        print("\nmorelorem.txt")
-        print(out)
-        self.assertIn("Successfully wrote to ", out)
+        # def test_write_morelorem(self):
+        #     out = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+        #     print("\nmorelorem.txt")
+        #     print(out)
+        #     self.assertIn("Successfully wrote to ", out)
 
-    def test_write_temp(self):
-        out = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-        print("\ntmp/temp.txt")
+        # def test_write_temp(self):
+        #     out = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+        #     print("\ntmp/temp.txt")
+        #     print(out)
+        #     self.assertIn("Error: ", out)
+
+    # run file
+    def test_run_main(self):
+        out = run_python_file("calculator", "main.py")
+        print("\nmain/")
         print(out)
-        self.assertIn("Error: ", out)
+        #self.assertIn("Error: ", out)
+
+    def test_run_tests(self):
+        out = run_python_file("calculator", "tests.py")
+        print("\ntests.py")
+        print(out)
+        #self.assertIn("Error: ", out)
+
+    def test_run_parent(self):
+        out = run_python_file("calculator", "../main.py")
+        print("\n../main.py")
+        print(out)
+        #self.assertIn("Error: ", out)
+
+    def test_run_non(self):
+        out = run_python_file("calculator", "nonexistant.py")
+        print("\nnonexistant.py")
+        print(out)
+        #self.assertIn("Error: ", out)
 
 if __name__ == "__main__":
     unittest.main()
